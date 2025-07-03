af99885bc7cc005b219453109d4a372141581c79
pedrosnaves <pedrosnaves@gmail.com>
2025-07-03 20:55:37 -0300

    Update Battery.java

--- Battery.java
+++ Battery.java
@@ -1,27 +1,29 @@
 package br.pedro.smartgrid;
 
-public class Battery implements EnergyComponent {
+// Classe folha do Composite, implementando Flyweight
+
+public class Battery implements EnergyComponent { // Referência compartilhada a DeviceSpecs usando o Flyweight
 
     private final DeviceFactory.DeviceSpecs specs;
-    private double stored;                 // kWh
+    private double stored;                 
 
     public Battery(DeviceFactory.DeviceSpecs specs) {
         this.specs = specs;
         this.stored = 0;
     }
 
-    /* ================= UTILITÁRIOS =================== */
+    
     public double getCapacity()          { return specs.capacity; }
     public double getStateOfCharge()     { return stored / specs.capacity; }
 
-    /** carrega e devolve quanto realmente aceitou */
+   
     public double charge(double kWh) {
         double aceito = Math.min(kWh, specs.capacity - stored);
         stored += aceito;
         return aceito;
     }
 
-    /** descarrega e devolve quanto realmente forneceu */
+    
     public double discharge(double kWh) {
     double fornecido = Math.min(kWh, stored);
     stored -= fornecido;
@@ -30,9 +32,12 @@ public class Battery implements EnergyComponent {
     return fornecido;
 }
 
-    /* ============== IMPLEMENTAÇÃO DO COMPONENT ============== */
+  
    @Override
-public double simulateTick(int tick, double irradiance) { return 0; }
+public double simulateTick(int tick, double irradiance) { // Implementação do método obrigatório de simulação
+    return 0;
+}
 
-    @Override public double getStoredEnergy()                          { return stored; }
+    @Override public double getStoredEnergy() {
+        return stored; }
 }
