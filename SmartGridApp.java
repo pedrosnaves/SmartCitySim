190dce0a726eb141f2ec081afcbaaff8c135c851
pedrosnaves <pedrosnaves@gmail.com>
2025-07-03 21:14:02 -0300

    Update SmartGridApp.java

--- SmartGridApp.java
+++ SmartGridApp.java
@@ -6,26 +6,26 @@ import java.nio.file.Path;
 public class SmartGridApp {
 
     public static void main(String[] args) {
-        try {
-            // ① procura devices.csv no diretório de execução (CWD)
+        try { // tratamento de excessões
+           
             Path csv = Path.of("devices.csv");
 
-            // ② se não achar, tenta um nível acima (raiz do projeto)
-            if (!Files.exists(csv)) {
+           
+            if (!Files.exists(csv)) { // Se não encontrado, tenta novamente no diretório pai
                 csv = Path.of(System.getProperty("user.dir"))
                           .getParent()          // sobe 1 diretório
                           .resolve("devices.csv");
             }
 
-            System.out.println("CSV que vou usar: " + csv.toAbsolutePath());
+            System.out.println("CSV que vou usar: " + csv.toAbsolutePath()); // Linha para imprimir o diretório absoluto do arquivo csv, para ser encontrado manualmente se necessário
 
             /* ---------------------------------------------------------------- */
-            CompositeEnergyUnit grid = DeviceFactory.loadGridFromCsv(csv);
-            SimulationManager manager = SimulationManager.getInstance();
+            CompositeEnergyUnit grid = DeviceFactory.loadGridFromCsv(csv); // Carrega a estrutura Composite completa atravéz do Factory Method
+            SimulationManager manager = SimulationManager.getInstance(); // Inicializa a simulação (Singleton), configurando-a com a árvore recém carregada
             manager.init(grid);
 
-            new SimpleView();          // GUI
-            new TickerThread().start(); // relógio em thread separada
+            new SimpleView();          // Cria a GUI através do Simpleview, mostrando a aplicação funcionando visualmente
+            new TickerThread().start(); // Inicia a thread que controla a passagem de tempo na simulação
             /* ---------------------------------------------------------------- */
 
         } catch (Exception e) {
